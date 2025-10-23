ccc2645a8e66ff4baca4a4adc4bc90891df57f92
lalit_easypool <easypoolsol@gmail.com>
2025-10-23 10:35:31 +0530

    Fix: Relocate maps_config.dart to resolve analysis error

---
+++ lib/features/map/widgets/maps_config.dart
@@ -0,0 +1,6 @@
+const String googleMapsApiKey = String.fromEnvironment('GOOGLE_MAPS_API_KEY', defaultValue: 'YOUR_API_KEY_HERE');
+
+class HomeLocation {
+  static const double latitude = 0.0;
+  static const double longitude = 0.0;
+}
--- lib/features/map/widgets/route_map_widget.dart
+++ lib/features/map/widgets/route_map_widget.dart
@@ -6,7 +6,7 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:latlong2/latlong.dart';
 
-import 'package:frontend_easy/core/config/maps_config.dart';
+import 'maps_config.dart';
 import 'package:frontend_easy/core/theme/route_colors.dart';
 import 'package:frontend_easy/features/fleet/models/map_mode.dart';
 import 'package:frontend_easy/features/fleet/providers/bus_locations_provider.dart';
