c61c7e16ac8fc355bf25bdf7c96792ddae544619
lalit_easypool <easypoolsol@gmail.com>
2025-10-24 13:55:14 +0530

    chore: Remove unused Dio import from login screen

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:dio/dio.dart';
 
 import 'package:frontend_easy/shared/services/api_service.dart';
 import 'package:frontend_easy/shared/services/auth_service.dart';
